657af7606470662e2616548dc5d062f7d7225f77
suthyscott <sutherlandscott21@gmail.com>
2023-03-15 15:59:18 -0600

    added details routing button

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,6 +7,8 @@ const Details = () => {
   return (
     <div>Details
       <p>{displayArticle.title}</p>
+      <img src={displayArticle.urlToImage}/>
+      <a href={displayArticle.url} target="_blank">See full article</a>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 import { selectPotentials } from '../redux/potentialArticlesSlice'
+import { setDisplayArticle } from '../redux/displayArticleSlice'
 import './Home.css'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   const potentialArticles = useSelector(selectPotentials)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-
+  const selectArticleHandler = article => {
+    dispatch(setDisplayArticle(article))
+    navigate('/details')
+  }
 
   console.log(potentialArticles)
 
@@ -15,11 +22,12 @@ const Home = () => {
       <div className='article-container'>
         <h1>{article.title}</h1>
         <img src={article.urlToImage}/>
+        <button onClick={() => selectArticleHandler(article)}>See more</button>
       </div>
     ) : null
   })
   return (
-    <div>{articlesDisplay}</div>
+    <div>{potentialArticles.length === 0 ? 'There are no reults for that search term' : articlesDisplay}</div>
   )
 }
 
