4e939497739506035c28ca930405acf37780324e
suthyscott <sutherlandscott21@gmail.com>
2023-03-15 09:54:26 -0600

    accessing and displaying articles from redux in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectPotentials } from '../redux/potentialArticlesSlice'
+import './Home.css'
 
 const Home = () => {
+  const potentialArticles = useSelector(selectPotentials)
+
+
+
+  console.log(potentialArticles)
+
+  const articlesDisplay = potentialArticles.map(article => {
+    return article.urlToImage ? (
+      <div className='article-container'>
+        <h1>{article.title}</h1>
+        <img src={article.urlToImage}/>
+      </div>
+    ) : null
+  })
   return (
-    <div>Home</div>
+    <div>{articlesDisplay}</div>
   )
 }
 
